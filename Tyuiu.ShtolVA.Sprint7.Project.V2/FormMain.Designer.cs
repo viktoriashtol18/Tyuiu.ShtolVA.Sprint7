d3c04b30739c18aa41e9a498aea6bc0585cd88e9
viktoriashtol18 <viktoriastol878@gmail.com>
2023-12-17 23:28:39 +0500

    Project.V2

--- Tyuiu.ShtolVA.Sprint7.Project.V2/FormMain.Designer.cs
+++ Tyuiu.ShtolVA.Sprint7.Project.V2/FormMain.Designer.cs
@@ -29,21 +29,35 @@ namespace Tyuiu.ShtolVA.Sprint7.Project.V2
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1086, 284);
+            this.textBox1.TabIndex = 0;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1110, 669);
+            this.Controls.Add(this.textBox1);
             this.Name = "FormMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
