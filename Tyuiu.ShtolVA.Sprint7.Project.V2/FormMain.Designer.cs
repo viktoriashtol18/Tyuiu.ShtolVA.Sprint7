b42470fb01d60ca7ac7899c8856f562bd7e34d6b
viktoriashtol18 <viktoriastol878@gmail.com>
2023-12-23 23:58:20 +0500

    Project.V2

--- Tyuiu.ShtolVA.Sprint7.Project.V2/FormMain.Designer.cs
+++ Tyuiu.ShtolVA.Sprint7.Project.V2/FormMain.Designer.cs
@@ -29,34 +29,257 @@ namespace Tyuiu.ShtolVA.Sprint7.Project.V2
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel_SVA = new System.Windows.Forms.Panel();
+            this.panelBottom_SVA = new System.Windows.Forms.Panel();
+            this.buttonFile_SVA = new System.Windows.Forms.Button();
+            this.buttonGraf_SVA = new System.Windows.Forms.Button();
+            this.buttonQuickGuide_SVA = new System.Windows.Forms.Button();
+            this.buttonProgramm_SVA = new System.Windows.Forms.Button();
+            this.groupBoxStatic_SVA = new System.Windows.Forms.GroupBox();
+            this.labelKolvo_SVA = new System.Windows.Forms.Label();
+            this.labelSum_SVA = new System.Windows.Forms.Label();
+            this.labelAverage_SVA = new System.Windows.Forms.Label();
+            this.labelMin_SVA = new System.Windows.Forms.Label();
+            this.labelMax_SVA = new System.Windows.Forms.Label();
+            this.textBoxKolvo_SVA = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxAverage_SVA = new System.Windows.Forms.TextBox();
+            this.textBoxMin_SVA = new System.Windows.Forms.TextBox();
+            this.textBoxMax_SVA = new System.Windows.Forms.TextBox();
+            this.buttonAddition_SVA = new System.Windows.Forms.Button();
+            this.buttonDelete_SVA = new System.Windows.Forms.Button();
+            this.panel_SVA.SuspendLayout();
+            this.panelBottom_SVA.SuspendLayout();
+            this.groupBoxStatic_SVA.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel_SVA
+            // 
+            this.panel_SVA.BackColor = System.Drawing.Color.Linen;
+            this.panel_SVA.Controls.Add(this.buttonProgramm_SVA);
+            this.panel_SVA.Controls.Add(this.buttonQuickGuide_SVA);
+            this.panel_SVA.Controls.Add(this.buttonGraf_SVA);
+            this.panel_SVA.Controls.Add(this.buttonFile_SVA);
+            this.panel_SVA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_SVA.Location = new System.Drawing.Point(0, 0);
+            this.panel_SVA.Name = "panel_SVA";
+            this.panel_SVA.Size = new System.Drawing.Size(1253, 705);
+            this.panel_SVA.TabIndex = 0;
+            // 
+            // panelBottom_SVA
+            // 
+            this.panelBottom_SVA.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panelBottom_SVA.Controls.Add(this.buttonDelete_SVA);
+            this.panelBottom_SVA.Controls.Add(this.buttonAddition_SVA);
+            this.panelBottom_SVA.Controls.Add(this.groupBoxStatic_SVA);
+            this.panelBottom_SVA.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom_SVA.Location = new System.Drawing.Point(0, 529);
+            this.panelBottom_SVA.Name = "panelBottom_SVA";
+            this.panelBottom_SVA.Size = new System.Drawing.Size(1253, 176);
+            this.panelBottom_SVA.TabIndex = 1;
+            // 
+            // buttonFile_SVA
+            // 
+            this.buttonFile_SVA.Location = new System.Drawing.Point(12, 12);
+            this.buttonFile_SVA.Name = "buttonFile_SVA";
+            this.buttonFile_SVA.Size = new System.Drawing.Size(90, 29);
+            this.buttonFile_SVA.TabIndex = 0;
+            this.buttonFile_SVA.Text = "Файл";
+            this.buttonFile_SVA.UseVisualStyleBackColor = true;
+            // 
+            // buttonGraf_SVA
+            // 
+            this.buttonGraf_SVA.Location = new System.Drawing.Point(108, 12);
+            this.buttonGraf_SVA.Name = "buttonGraf_SVA";
+            this.buttonGraf_SVA.Size = new System.Drawing.Size(85, 29);
+            this.buttonGraf_SVA.TabIndex = 1;
+            this.buttonGraf_SVA.Text = "Графики";
+            this.buttonGraf_SVA.UseVisualStyleBackColor = true;
+            // 
+            // buttonQuickGuide_SVA
+            // 
+            this.buttonQuickGuide_SVA.Location = new System.Drawing.Point(200, 12);
+            this.buttonQuickGuide_SVA.Name = "buttonQuickGuide_SVA";
+            this.buttonQuickGuide_SVA.Size = new System.Drawing.Size(161, 29);
+            this.buttonQuickGuide_SVA.TabIndex = 2;
+            this.buttonQuickGuide_SVA.Text = "Краткое руководство\r\n";
+            this.buttonQuickGuide_SVA.UseVisualStyleBackColor = true;
+            // 
+            // buttonProgramm_SVA
+            // 
+            this.buttonProgramm_SVA.Location = new System.Drawing.Point(367, 12);
+            this.buttonProgramm_SVA.Name = "buttonProgramm_SVA";
+            this.buttonProgramm_SVA.Size = new System.Drawing.Size(119, 28);
+            this.buttonProgramm_SVA.TabIndex = 3;
+            this.buttonProgramm_SVA.Text = "О программе";
+            this.buttonProgramm_SVA.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxStatic_SVA
+            // 
+            this.groupBoxStatic_SVA.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBoxStatic_SVA.Controls.Add(this.textBoxMax_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.textBoxMin_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.textBoxAverage_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.textBox1);
+            this.groupBoxStatic_SVA.Controls.Add(this.textBoxKolvo_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.labelMax_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.labelMin_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.labelAverage_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.labelSum_SVA);
+            this.groupBoxStatic_SVA.Controls.Add(this.labelKolvo_SVA);
+            this.groupBoxStatic_SVA.Location = new System.Drawing.Point(0, 3);
+            this.groupBoxStatic_SVA.Name = "groupBoxStatic_SVA";
+            this.groupBoxStatic_SVA.Size = new System.Drawing.Size(915, 172);
+            this.groupBoxStatic_SVA.TabIndex = 0;
+            this.groupBoxStatic_SVA.TabStop = false;
+            this.groupBoxStatic_SVA.Text = "Статичечкме данные";
+            // 
+            // labelKolvo_SVA
+            // 
+            this.labelKolvo_SVA.AutoSize = true;
+            this.labelKolvo_SVA.BackColor = System.Drawing.Color.Linen;
+            this.labelKolvo_SVA.Location = new System.Drawing.Point(6, 27);
+            this.labelKolvo_SVA.Name = "labelKolvo_SVA";
+            this.labelKolvo_SVA.Size = new System.Drawing.Size(127, 17);
+            this.labelKolvo_SVA.TabIndex = 0;
+            this.labelKolvo_SVA.Text = "Количество строк";
+            // 
+            // labelSum_SVA
+            // 
+            this.labelSum_SVA.AutoSize = true;
+            this.labelSum_SVA.BackColor = System.Drawing.Color.Linen;
+            this.labelSum_SVA.Location = new System.Drawing.Point(154, 27);
+            this.labelSum_SVA.Name = "labelSum_SVA";
+            this.labelSum_SVA.Size = new System.Drawing.Size(50, 17);
+            this.labelSum_SVA.TabIndex = 1;
+            this.labelSum_SVA.Text = "Сумма";
+            // 
+            // labelAverage_SVA
+            // 
+            this.labelAverage_SVA.AutoSize = true;
+            this.labelAverage_SVA.BackColor = System.Drawing.Color.Linen;
+            this.labelAverage_SVA.Location = new System.Drawing.Point(274, 27);
+            this.labelAverage_SVA.Name = "labelAverage_SVA";
+            this.labelAverage_SVA.Size = new System.Drawing.Size(136, 17);
+            this.labelAverage_SVA.TabIndex = 2;
+            this.labelAverage_SVA.Text = "Среднее значение ";
+            // 
+            // labelMin_SVA
+            // 
+            this.labelMin_SVA.AutoSize = true;
+            this.labelMin_SVA.BackColor = System.Drawing.Color.Linen;
+            this.labelMin_SVA.Location = new System.Drawing.Point(445, 27);
+            this.labelMin_SVA.Name = "labelMin_SVA";
+            this.labelMin_SVA.Size = new System.Drawing.Size(166, 17);
+            this.labelMin_SVA.TabIndex = 3;
+            this.labelMin_SVA.Text = "Минимальное значение";
+            // 
+            // labelMax_SVA
+            // 
+            this.labelMax_SVA.AutoSize = true;
+            this.labelMax_SVA.BackColor = System.Drawing.Color.Linen;
+            this.labelMax_SVA.Location = new System.Drawing.Point(638, 27);
+            this.labelMax_SVA.Name = "labelMax_SVA";
+            this.labelMax_SVA.Size = new System.Drawing.Size(172, 17);
+            this.labelMax_SVA.TabIndex = 4;
+            this.labelMax_SVA.Text = "Максимальное значение";
+            // 
+            // textBoxKolvo_SVA
+            // 
+            this.textBoxKolvo_SVA.Location = new System.Drawing.Point(6, 56);
+            this.textBoxKolvo_SVA.Multiline = true;
+            this.textBoxKolvo_SVA.Name = "textBoxKolvo_SVA";
+            this.textBoxKolvo_SVA.Size = new System.Drawing.Size(96, 22);
+            this.textBoxKolvo_SVA.TabIndex = 5;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(148, 56);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(1086, 284);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.Size = new System.Drawing.Size(94, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBoxAverage_SVA
+            // 
+            this.textBoxAverage_SVA.Location = new System.Drawing.Point(277, 56);
+            this.textBoxAverage_SVA.Name = "textBoxAverage_SVA";
+            this.textBoxAverage_SVA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAverage_SVA.TabIndex = 7;
+            // 
+            // textBoxMin_SVA
+            // 
+            this.textBoxMin_SVA.Location = new System.Drawing.Point(448, 55);
+            this.textBoxMin_SVA.Name = "textBoxMin_SVA";
+            this.textBoxMin_SVA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMin_SVA.TabIndex = 8;
+            // 
+            // textBoxMax_SVA
+            // 
+            this.textBoxMax_SVA.Location = new System.Drawing.Point(641, 55);
+            this.textBoxMax_SVA.Name = "textBoxMax_SVA";
+            this.textBoxMax_SVA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMax_SVA.TabIndex = 9;
+            // 
+            // buttonAddition_SVA
+            // 
+            this.buttonAddition_SVA.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAddition_SVA.Location = new System.Drawing.Point(1015, 41);
+            this.buttonAddition_SVA.Name = "buttonAddition_SVA";
+            this.buttonAddition_SVA.Size = new System.Drawing.Size(142, 39);
+            this.buttonAddition_SVA.TabIndex = 1;
+            this.buttonAddition_SVA.Text = "Добавить";
+            this.buttonAddition_SVA.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete_SVA
+            // 
+            this.buttonDelete_SVA.Location = new System.Drawing.Point(1015, 99);
+            this.buttonDelete_SVA.Name = "buttonDelete_SVA";
+            this.buttonDelete_SVA.Size = new System.Drawing.Size(142, 40);
+            this.buttonDelete_SVA.TabIndex = 2;
+            this.buttonDelete_SVA.Text = "Удалить";
+            this.buttonDelete_SVA.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1110, 669);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1253, 705);
+            this.Controls.Add(this.panelBottom_SVA);
+            this.Controls.Add(this.panel_SVA);
+            this.MinimumSize = new System.Drawing.Size(1128, 716);
             this.Name = "FormMain";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сеть магазинов";
             this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel_SVA.ResumeLayout(false);
+            this.panelBottom_SVA.ResumeLayout(false);
+            this.groupBoxStatic_SVA.ResumeLayout(false);
+            this.groupBoxStatic_SVA.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel_SVA;
+        private System.Windows.Forms.Panel panelBottom_SVA;
+        private System.Windows.Forms.Button buttonFile_SVA;
+        private System.Windows.Forms.Button buttonQuickGuide_SVA;
+        private System.Windows.Forms.Button buttonGraf_SVA;
+        private System.Windows.Forms.Button buttonProgramm_SVA;
+        private System.Windows.Forms.GroupBox groupBoxStatic_SVA;
+        private System.Windows.Forms.TextBox textBoxKolvo_SVA;
+        private System.Windows.Forms.Label labelMax_SVA;
+        private System.Windows.Forms.Label labelMin_SVA;
+        private System.Windows.Forms.Label labelAverage_SVA;
+        private System.Windows.Forms.Label labelSum_SVA;
+        private System.Windows.Forms.Label labelKolvo_SVA;
+        private System.Windows.Forms.Button buttonDelete_SVA;
+        private System.Windows.Forms.Button buttonAddition_SVA;
+        private System.Windows.Forms.TextBox textBoxMax_SVA;
+        private System.Windows.Forms.TextBox textBoxMin_SVA;
+        private System.Windows.Forms.TextBox textBoxAverage_SVA;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
